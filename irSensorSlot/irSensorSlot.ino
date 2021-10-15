806751547097fb7ffe12f87a3a9b57e42c1de03b
Alberto Reales <reales_101@hotmail.com>
2021-10-15 18:25:03 +0200

    Added support for 10 different sensors

--- irSensorSlot/irSensorSlot.ino
+++ irSensorSlot/irSensorSlot.ino
@@ -1,8 +1,17 @@
-const int redLedPin = 8;
-const int greenLedPin = 7;
-const int sensorPin = 12;
+const int redLedPin = 2;
+const int greenLedPin = 3;
+const int sensor1Pin = A0;
+const int sensor2Pin = A1;
+const int sensor3Pin = A2;
+const int sensor4Pin = A3;
+const int sensor5Pin = A4;
+const int sensor6Pin = A5;
+const int sensor7Pin = 4;
+const int sensor8Pin = 5;
+const int sensor9sPin = 6;
+const int sensor10Pin = 7;
 
-const long detectionThreshold = 600;
+const long detectionThreshold = 700;
 
 unsigned long detectionTime; 
 
@@ -12,27 +21,36 @@ void setup() {
 
   Serial.begin(9600);
   
+  // Setting the leds
   pinMode(redLedPin, OUTPUT);
   pinMode(greenLedPin, OUTPUT);
 
-  pinMode(sensorPin, INPUT);
-
   digitalWrite(greenLedPin, HIGH);
   digitalWrite(redLedPin, LOW); 
+
+  // Setting the sensors
+  pinMode(sensor1Pin, INPUT);
+  pinMode(sensor2Pin, INPUT);
+  pinMode(sensor3Pin, INPUT);
+  pinMode(sensor4Pin, INPUT);
+  pinMode(sensor5Pin, INPUT);
+  pinMode(sensor6Pin, INPUT);
+  pinMode(sensor7Pin, INPUT);
+  pinMode(sensor8Pin, INPUT);
+  pinMode(sensor9sPin, INPUT);
+  pinMode(sensor10Pin, INPUT);
 }
+
+boolean isCarDetected() {
+  return !digitalRead(sensor1Pin) || !digitalRead(sensor2Pin) || !digitalRead(sensor3Pin) || !digitalRead(sensor4Pin) || !digitalRead(sensor5Pin) || !digitalRead(sensor6Pin) || !digitalRead(sensor7Pin) || !digitalRead(sensor8Pin) || !digitalRead(sensor9sPin) || !digitalRead(sensor10Pin);
+}
+
 
 void loop() {
 
-  Serial.println("Hello World");
-
   // No car detected on the previous loop
   if(!isDetected) {
-    boolean carDetected = !digitalRead(sensorPin);
-
-    Serial.println("Inside first if");
-
-    Serial.print("Car detected value: ");
-    Serial.println(carDetected);
+    boolean carDetected = isCarDetected();
 
     // Car detected on this loop
     if(carDetected) {
@@ -40,36 +58,28 @@ void loop() {
 
       detectionTime = millis();
 
-      Serial.print("Sensor activated: "); 
+      Serial.print("Car detected: "); 
       Serial.println(detectionTime);
     }
   } else { // Car detected on the previous loop
-
-    Serial.println("Inside first else");
     
     // Check if the car is still there
-    boolean carDetected = !digitalRead(sensorPin);
-
-    Serial.print("Car detected value: ");
-    Serial.println(carDetected);
+    boolean carDetected = isCarDetected();
     
     if(carDetected) {
       unsigned long detectionPeriod = millis() - detectionTime;
-
-      Serial.print("detectionPeriod value: ");
-      Serial.println(detectionPeriod);
       
       // The car is stopped in front of the sensor 
       if(detectionPeriod > detectionThreshold) {
         digitalWrite(greenLedPin, LOW);
         digitalWrite(redLedPin, HIGH); 
 
-        Serial.print("Car detected");
-        Serial.println(detectionPeriod);
       }  
     } else {
       digitalWrite(greenLedPin, HIGH);
       digitalWrite(redLedPin, LOW); 
+
+      Serial.print("Car not detected");
       
       isDetected = false;
     }
